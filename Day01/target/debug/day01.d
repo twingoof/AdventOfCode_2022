c1b3b9cb6766f1b7b03676159b1ba45a590c5d45
twingoof <twingood@proton.me>
2022-12-12 00:47:12 -0500

    :truck: Renaming day in snake_case

---
+++ Day01/target/debug/day01.d
@@ -0,0 +1,1 @@
+/Users/ppr/Dev/AdventOfCode_2022/day01/target/debug/day01: /Users/ppr/Dev/AdventOfCode_2022/day01/src/main.rs
---
+++ Day01/target/debug/deps/day01-1f4270d54ca9fe7a.d
@@ -0,0 +1,5 @@
+/Users/ppr/Dev/AdventOfCode_2022/day01/target/debug/deps/day01-1f4270d54ca9fe7a: src/main.rs
+
+/Users/ppr/Dev/AdventOfCode_2022/day01/target/debug/deps/day01-1f4270d54ca9fe7a.d: src/main.rs
+
+src/main.rs:
